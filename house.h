0d2db2b6e2ff7569cd046324c7a6a0bc0149960a
Vlaffy <92908950+quanvladimir@users.noreply.github.com>
2022-12-26 23:02:58 +0700

    Upload House.cpp and House.h

---
+++ house.h
@@ -0,0 +1,46 @@
+
+#ifndef HOUSE_H
+#define HOUSE_H
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+class House {
+protected:
+    string houseID;
+    string startDate;
+    string endDate;
+    string address;
+    string location;
+    string description;
+    std::vector<double> houseRatings;
+    bool status;
+    std::vector<Member> occupiers;
+    std::vector<Member> requestList;
+    std::map<string, string> occupierComment;
+public:
+    friend class Member;
+    friend class Admin;
+    House();
+
+
+
+    void showFullHouse();
+
+    void showDemoHouse();
+
+    double avgScore();
+
+
+};
+
+
+#endif //HOUSE_H
