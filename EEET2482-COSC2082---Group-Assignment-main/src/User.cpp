a15b908b630138e5e886970b8304fd981919ec83
Thach-s3927021 <93109265+NgoTranBaoThach@users.noreply.github.com>
2022-12-28 02:27:35 +0700

    Delete EEET2482-COSC2082---Group-Assignment-main directory

--- EEET2482-COSC2082---Group-Assignment-main/src/User.cpp
+++
@@ -1,9 +0,0 @@
-#include "User.h"
-
-User::User() = default;
-
-// User::User(string id, string username, string password) {
-//     this->id = id;
-//     this->password = password;
-//     this->username = username;
-// }
